f290b3103dd0350c1fd5756bf76835e454337678
Alex Wang <se795646@gmail.com>
2026-07-24 20:08:00 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,18 +1,16 @@
+import { createApp } from 'vue';
 import App from './App.vue';
 import store from './store/index.js';
 import router from './router';
 import vuetify from './plugins/vuetify';
 import i18n from './i18n';
-import bootstrapApp from '@/foundation/bootstrap/bootstrapApp';
 import './assets/css/tailwind.css';
 
-const CONFIG_URL = `config/serverInfo.json?version=${__VERSION__}`; // eslint-disable-line no-undef
+const app = createApp(App);
 
-bootstrapApp({
-    App,
-    configUrl: CONFIG_URL,
-    plugins: [router, vuetify, i18n, store]
-}).catch((error) => {
-    // eslint-disable-next-line no-console
-    console.error('Failed to load app config:', error);
-});
+app.use(router);
+app.use(vuetify);
+app.use(i18n);
+app.use(store);
+
+app.mount('#app');
